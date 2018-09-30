56004d0514062ae7003eb9619bcb41c2f867ce2b
Dan Cartier <nospamdan1@gmail.com>
2018-09-30 05:45:02 -0400

    msm8974-common: Disable HDR 1x extra, zero-exposure frame
    * The logic was backwards... meh
    * Match need_hdr_1x parameter to the local variable name
    * Disable this mode for good

--- camera/QCamera2/HAL/QCameraParameters.cpp
+++ camera/QCamera2/HAL/QCameraParameters.cpp
@@ -782,7 +782,7 @@ QCameraParameters::QCameraParameters(const String8 &params)
     m_bHDREnabled(false),
     m_bAVTimerEnabled(false),
     m_AdjustFPS(NULL),
-    m_bHDR1xFrameEnabled(true),
+    m_bHDR1xFrameEnabled(false),
     m_HDRSceneEnabled(false),
     m_bHDRThumbnailProcessNeeded(false),
     m_bHDR1xExtraBufferNeeded(true),
@@ -3020,12 +3020,9 @@ int32_t QCameraParameters::setSceneMode(const QCameraParameters& params)
                 // set if hdr 1x image is needed
                 const char *need_hdr_1x = params.get(KEY_QC_HDR_NEED_1X);
                 if (need_hdr_1x != NULL) {
-                    if (strcmp(need_hdr_1x, VALUE_TRUE) == 0) {
-                        m_bHDR1xFrameEnabled = true;
-                    } else {
-                        m_bHDR1xFrameEnabled = false;
-                    }
 
+                    m_bHDR1xFrameEnabled = false;
+                    
                     updateParamEntry(KEY_QC_HDR_NEED_1X, need_hdr_1x);
                 }
 
@@ -4442,8 +4439,6 @@ int32_t QCameraParameters::initDefaultParameters()
     set(KEY_QC_SUPPORTED_SCENE_DETECT, onOffValues);
     setSceneDetect(VALUE_OFF);
     m_bHDREnabled = false;
-    m_bHDR1xFrameEnabled = true;
-
     m_bHDRThumbnailProcessNeeded = false;
     m_bHDR1xExtraBufferNeeded = true;
     for (uint32_t i=0; i<m_pCapability->hdr_bracketing_setting.num_frames; i++) {
